5681c478d7c56bd54b634f1b8b2de71e31d861fd
Mason3144 <lts890303@gmail.com>
2023-05-15 11:38:12 +0900

    Env for H2 DB

--- src/main/java/com/boardProject/member/controller/MemberController.java
+++ src/main/java/com/boardProject/member/controller/MemberController.java
@@ -1,2 +1,71 @@
-package com.boardProject.member.controller;public class MemberController {
+package com.boardProject.member.controller;
+
+import com.boardProject.dto.SingleResponseDto;
+import com.boardProject.member.dto.MemberDto;
+import com.boardProject.member.entity.Member;
+import com.boardProject.member.mapper.MemberMapper;
+import com.boardProject.member.service.MemberService;
+import com.boardProject.utils.UriCreator;
+import org.mapstruct.control.MappingControl;
+import org.mapstruct.control.MappingControls;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/members")
+@Validated
+public class MemberController {
+    private final static String MEMBER_DEFAULT_URL = "/v1/members";
+    private final MemberService service;
+    private final MemberMapper mapper;
+
+    public MemberController(MemberService service, MemberMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postMember(@RequestBody @Valid MemberDto.Post requestBody){
+        // email verification needed
+        // social login create member needed
+        Member memberCreated = service.createMember(mapper.memberPostToMember(requestBody));
+
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL,memberCreated.getMemberId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
+                                      @RequestBody @Valid MemberDto.Patch requestBody){
+        // profile photo update needed
+        requestBody.setMemberId(memberId);
+
+        Member updatedMember = service.updateMember(mapper.memberPatchToMember(requestBody));
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.memberToMemberResponse(updatedMember)), HttpStatus.OK
+        );
+    }
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId){
+        Member foundMember = service.findMember(memberId);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.memberToMemberResponse(foundMember)),HttpStatus.OK
+        );
+    }
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") @Positive long memberId){
+        Member deletedMember = service.deleteMember(memberId);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.memberToMemberResponse(deletedMember)), HttpStatus.OK
+        );
+    }
 }
--- src/main/java/com/boardProject/member/service/MemberService.java
+++ src/main/java/com/boardProject/member/service/MemberService.java
@@ -1,2 +1,61 @@
-package com.boardProject.member.service;public class MemberService {
+package com.boardProject.member.service;
+
+import com.boardProject.exception.businessLogicException.BusinessLogicException;
+import com.boardProject.exception.businessLogicException.ExceptionCode;
+import com.boardProject.member.entity.Member;
+import com.boardProject.member.repository.MemberRepository;
+import com.boardProject.utils.CustomBeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class MemberService {
+    private final MemberRepository repository;
+    private final CustomBeanUtils<Member> customBeanUtils;
+
+    public MemberService(MemberRepository repository, CustomBeanUtils<Member> customBeanUtils) {
+        this.repository = repository;
+        this.customBeanUtils = customBeanUtils;
+    }
+
+    public Member createMember(Member member){
+        // email verification needed
+        // social login create member needed
+
+        existsEmailChecker(member.getEmail());
+        return repository.save(member);
+    }
+
+    public Member updateMember(Member member){
+        // profile photo update needed
+        Member foundMember = findExistsMember(member.getMemberId());
+
+        Member updateMember = customBeanUtils.copyNonNullProperties(member,foundMember);
+
+        return repository.save(updateMember);
+    }
+
+    public Member findMember(long memberId){
+        return findExistsMember(memberId);
+    }
+
+    public Member deleteMember(long memberId){
+        Member member = findExistsMember(memberId);
+        member.setMemberStatus(Member.MemberStatus.MEMBER_QUIT);
+
+        return repository.save(member);
+    }
+
+    private void existsEmailChecker(String email){
+        Optional<Member> member = repository.findByEmail(email);
+        if(member.isPresent()) throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+    }
+
+    private Member findExistsMember(long memberId){
+        Optional<Member> optionalMember = repository.findById(memberId);
+        return optionalMember.orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+    }
 }
