a9f7298a4ddf94263ffa1ebb5f2f7f798d2650b4
Mason3144 <lts890303@gmail.com>
2023-05-30 21:42:35 +0900

    bug fixed for verifying not loggedin member

--- src/main/java/com/boardProject/utils/LoggedInMemberUtils.java
+++ src/main/java/com/boardProject/utils/LoggedInMemberUtils.java
@@ -5,13 +5,22 @@ import com.boardProject.exception.businessLogicException.BusinessLogicException;
 import com.boardProject.exception.businessLogicException.ExceptionCode;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Objects;
+
 public class LoggedInMemberUtils {
     public static JwtVerificationFilter.AuthenticatedPrincipal findLoggedInMember(){
-        return (JwtVerificationFilter.AuthenticatedPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(user instanceof JwtVerificationFilter.AuthenticatedPrincipal)
+            return (JwtVerificationFilter.AuthenticatedPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        else return null;
     }
     public static boolean verifyIsMineBoolean(long memberId){
         JwtVerificationFilter.AuthenticatedPrincipal loggedInMember = findLoggedInMember();
-        return loggedInMember.getMemberId() == memberId;
+        try{
+            return loggedInMember.getMemberId() == memberId;
+        }catch (NullPointerException e){
+            return false;
+        }
     }
 
     public static void verifyIsMineException(long memberId){
